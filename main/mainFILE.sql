6ab08be92520900904274a0107cd4cf6889ebde6
vfletes <vfletes@cusd.me>
2019-04-05 09:05:31 -0700

    update

--- main/mainFILE.sql
+++ main/mainFILE.sql
@@ -1,8 +1,22 @@
---this is my SQL project
+--this is my SQL cheat sheet
 CREATE TABLE animals(
     id INTEGER,
     type TEXT,
     age INTEGER);
 INSERT INTO animals (id, type, age)
 VALUES (1, 'dog', 3);
+INSERT INTO animals (id, type, age) 
+VALUES (2, 'cat', 2);
+ALTER TABLE animals ADD COLUMN gender TEXT;
+UPDATE animals SET gender = 'female' WHERE id = 1;
+DELETE FROM animals WHERE gender IS NULL;
 SELECT * FROM animals;
+SELECT 'type' FROM animals;
+SELECT * FROM animals WHERE age > 1 OR type = 'dog';
+SELECT * FROM animals WHERE type LIKE 'C__s' and age > 1;
+SELECT * FROM animals WHERE type LIKE '%s'; -- any amount of letters before 's'
+SELECT * FROM animals WHERE type BETWEEN 'a' AND 'e'; --type starting with a not including e
+
+
+--SELECT DISTINCT 'age' FROM animals; this means showing every id with age that
+--does not repeat and only showing the first use of a repeat
